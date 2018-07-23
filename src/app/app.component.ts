b89e65987199b9e091adeeed9dfb650af30fc593
Papapaya <s5811423123@pkru.ac.th>
2018-07-23 13:31:11 +0700

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My Word';
+  version = "1.0"
+
+  changeTitle(event){
+    console.log(event.target.value);
+    this.title = event.target.value;
+  }
+}
